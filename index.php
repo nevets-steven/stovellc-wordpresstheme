2a2f36809bec929119897d8b493060d80d30f1fc
nevets-steven <nevets322@gmail.com>
2025-04-30 15:36:21 -0400

    updating welcome image sizing and radius

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <div class="hero-container">
         <h1>Welcome to my website for STOVE LLC</h1>
         <!-- Welcome gif insert here -->
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/stovellc-welcome.png" alt="Welcoming Image">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/stovellc-welcome.png" alt="Welcoming Image" class='welcomepic'>
         
         <p>I design and build websites with clean code, thoughtful design, and a focus on usability. Whether you're here to explore my work, learn more about my background, or start a project — you're in the right place.</p>
     </div>
