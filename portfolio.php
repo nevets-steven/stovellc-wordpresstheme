c56a758f65ed978a05c48b500dd3d8e8a27663c8
nevets-steven <nevets322@gmail.com>
2025-05-02 12:28:24 -0400

    added rotating cube for portfolio item 1

--- portfolio.php
+++ portfolio.php
@@ -9,7 +9,7 @@ get_header();
     <div class="hero-container">
         <h1>Welcome to my portfolio</h1>
         <!-- Welcome gif insert here -->
-        <img src="https://picsum.photos/300" alt="Welcoming Gif">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/stovellc-welcome.png" alt="Welcoming Image" class='welcomepic'>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet itaque eligendi magni, hic sunt quisquam omnis iste animi tempora corporis.</p>
     </div>
     <button class="hero-cta" id="hero-cta">
@@ -21,7 +21,18 @@ get_header();
     <h2>Check this out!</h2>
     <p>This is the place where I want to showcase all that I can do, ranging from animations to cool CSS designs.</p>
 
-    <div class="porfolio-item item-1"></div>
+    <div class="porfolio-item item-1">
+        <div class="threed-cube-container">
+            <div class="cube">
+                <div class="face front">Front</div>
+                <div class="face back">Back</div>
+                <div class="face right">Right</div>
+                <div class="face left">Left</div>
+                <div class="face top">Top</div>
+                <div class="face bottom">Bottom</div>
+            </div>
+        </div>
+    </div>
     <div class="porfolio-item item-2"></div>
     <div class="porfolio-item item-3"></div>
     <div class="porfolio-item item-4"></div>
