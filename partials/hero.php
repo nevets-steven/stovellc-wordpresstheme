9e03202e7bd8cbe9e1393a83677492ac308073bc
nevets-steven <nevets322@gmail.com>
2025-05-02 13:34:13 -0400

    removed redundant code into own php file and added partials folder

---
+++ partials/hero.php
@@ -0,0 +1,13 @@
+<!-- partials/hero.php -->
+
+<section id="hero">
+    <div class="hero-container">
+        <h1>Welcome to my portfolio</h1>
+        <!-- Welcome gif insert here -->
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/stovellc-welcome.png" alt="Welcoming Image" class='welcomepic'>
+        <p>I design and build websites with clean code, thoughtful design, and a focus on usability. Whether you're here to explore my work, learn more about my background, or start a project — you're in the right place.</p>
+    </div>
+    <button class="hero-cta" id="hero-cta">
+        Click Me
+    </button>
+</section>
--- portfolio.php
+++ portfolio.php
@@ -10,7 +10,7 @@ get_header();
         <h1>Welcome to my portfolio</h1>
         <!-- Welcome gif insert here -->
         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/stovellc-welcome.png" alt="Welcoming Image" class='welcomepic'>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet itaque eligendi magni, hic sunt quisquam omnis iste animi tempora corporis.</p>
+        <p>I design and build websites with clean code, thoughtful design, and a focus on usability. Whether you're here to explore my work, learn more about my background, or start a project — you're in the right place.</p>
     </div>
     <button class="hero-cta" id="hero-cta">
         Click Me
