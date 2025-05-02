b99bcb1def6e05e67837c5f32b751d1caeef337b
nevets-steven <nevets322@gmail.com>
2025-05-02 13:34:42 -0400

    saved to update code

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
 <?php get_header(); ?>
 
-<section id="hero">
-    <div class="hero-container">
-        <h1>Welcome to my website for STOVE LLC</h1>
-        <!-- Welcome gif insert here -->
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/stovellc-welcome.png" alt="Welcoming Image" class='welcomepic'>
-        
-        <p>I design and build websites with clean code, thoughtful design, and a focus on usability. Whether you're here to explore my work, learn more about my background, or start a project — you're in the right place.</p>
-    </div>
-    <button class="hero-cta" id="hero-cta">
-        Click Me
-    </button>
-</section>
+<?php get_template_part('partials/hero'); ?>
 
 <section id="about">
     <div class="about-container">
