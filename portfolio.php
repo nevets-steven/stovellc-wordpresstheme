0803433396c4c1c645e8ff7029a06cab201d4092
nevets-steven <nevets322@gmail.com>
2025-06-10 11:01:54 -0400

    added link to porfolio.php

--- portfolio.php
+++ portfolio.php
@@ -10,6 +10,7 @@ get_header();
 <section id="portfolio">
     <h2>Check this out!</h2>
     <p>This is the place where I want to showcase all that I can do, ranging from animations to cool CSS designs.</p>
+    <p>Feel free to <a href='https://github.com/nevets-steven?tab=repositories' target='_blank' rel="noopener noreferrer">check out my GitHub</a> to see all the projects I have been doing over the past several years.</p>
 
     <div class="porfolio-item item-1">
         <div class="threed-cube-container">
