14dfa3f39dabfd4b7c5c9a68cb46560f7ebc0d80
nevets-steven <nevets322@gmail.com>
2025-06-16 20:49:15 -0400

    added scramble.php to navigation

--- header.php
+++ header.php
@@ -34,6 +34,7 @@
                 <a href="<?php echo site_url('/portfolio'); ?>">Portfolio</a>
                 <a href="<?php echo site_url('/#contact'); ?>">Contact</a>
                 <a href="https://nevets-steven.github.io/simon-says-capstone/" target="_blank" rel="noopener noreferrer">Simon Says</a>
+                <a href="<?php echo site_url('/scramble'); ?>">Contact</a>
             </div>
         </nav>
     </div>
